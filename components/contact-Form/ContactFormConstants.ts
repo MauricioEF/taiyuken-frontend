3fc675454be9df2d87b5ccce8e855c3e54c77383
Antonio-YKD <antonio67.ykd@gmail.com>
2026-04-24 16:46:36 -0600

    Feat : Contact Form

---
+++ components/contact-Form/ContactFormConstants.ts
@@ -0,0 +1,8 @@
+
+export const ContactFormConstants = [
+    { name : "Video Game"},
+    { name : "Board Game"},
+    { name : "Description"}
+]
+
+export const ValidationEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
