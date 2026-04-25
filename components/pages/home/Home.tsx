4e3f40e21746852cc2acc6a8221d4cfcc0447c17
Mauricio Espinosa <ing_mauricioespinosa@hotmail.com>
2026-04-24 22:57:31 -0600

    [Added next module to home]

--- components/pages/home/Home.tsx
+++ components/pages/home/Home.tsx
@@ -1,12 +1,13 @@
 import FeaturedProjects from '@/components/featured-projects';
 import Hero from '@/components/hero';
 import ContactForm from '@/components/contact-form';
+import OurWork from '@/components/our-work';
 
 const Home = () => {
   return (
     <>
       <Hero />
-      <FeaturedProjects/>
+      <OurWork/>
       <ContactForm/>
     </>
   );
